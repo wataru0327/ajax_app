90302288224f3cfb5cc48e3a1541606237cec2a6
wataru0327 <watarurataw00@icloud.com>
2025-08-19 18:24:19 +0900

    JSでレスポンスを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @posts = Post.order(id: "DESC")
   end
 
-  def create
+    def create
     post = Post.create(content: params[:content])
     render json:{ post: post }
   end
